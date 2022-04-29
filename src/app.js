fd97d2271fe4ec53583bd6b1005bf7bb8f5ede5a
Tiago Cabral <tiagorc.cabral@gmail.com>
2022-04-29 18:48:27 -0300

    :sparkles: adds cors and helmet libs

--- src/app.js
+++ src/app.js
@@ -1,13 +1,25 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const helmet = require('helmet');
+const cors = require('cors');
 const { sequelize } = require('./model')
-const { getProfile } = require('./middleware/getProfile')
 const routes = require('./routes/v1');
 // Set root path
 const app = express();
+
 app.use(bodyParser.json());
+
+// adds helmet HTTP protection
+app.use(helmet());
+
+// Sets CORS and Accepted Origins
+app.use(cors());
+app.options('*', cors());
+
 app.set('sequelize', sequelize)
+
 app.set('models', sequelize.models)
+
 app.use('/', routes)
 
 module.exports = app;
